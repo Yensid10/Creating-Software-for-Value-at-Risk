cca148c166d89bed02f24b5faffeef6a5cd069a9
Benjamin Shearlock <ben.shearlock@gmail.com>
2024-04-07 02:09:03 +0100

    Got Kivy Garden working + made first layout design
    I was having a big problem with getting the kivy garden matplotlib import to work, no matter how I tried to link it, the workspace could not see it. So, after searching online, I found some people talking about it, I used a pip install and then changed how I referenced the code, and I got it to work. So thats great, I now have it 100% working for me. So I set up my layout with the scrollable buttons like before, and I tested a fairly basic graph, it all seems to be working initially, idk if I'll be doing more on this today, we shall see.

--- Final Design/Screens/Graphs.py
+++ Final Design/Screens/Graphs.py
@@ -1,4 +1,23 @@
 from kivy.uix.screenmanager import Screen
 
+import matplotlib.pyplot as plt
+from kivy_garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+
+import logging
+logging.getLogger('matplotlib').setLevel(logging.INFO)
+
 class Graphs(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super(Graphs, self).__init__(**kwargs)
+        self.createGraph()
+
+    def createGraph(self):
+        plt.figure(figsize=(6, 4))
+        plt.plot([1, 29, 12, 4])
+        plt.ylabel('some random numbers')
+        plt.title('Random Graph')
+        plt.xlabel('more random numbers')
+
+        plt.tight_layout()
+
+        self.ids.graphSection.add_widget(FigureCanvasKivyAgg(plt.gcf()))
